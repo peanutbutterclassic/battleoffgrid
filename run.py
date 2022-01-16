3c865f3092b8c7521872658cce8ee0693f44fa2e
peanutbutterclassic <rano.r.s.fisher@gmail.com>
2022-01-16 00:17:35 +0000

    Add function for start game

--- run.py
+++ run.py
@@ -8,3 +8,12 @@ def battle_intro():
     else:
         print(f"Hello {name.capitalize()}, you are too young to play this game.")
 
+def enter_battle_perimeter():
+    print("YOU ARE ENTERING THE BATTLE PERIMETER!\n")
+
+def main():
+    battle_intro()
+
+
+if __name__ == '__main__':
+    main()
