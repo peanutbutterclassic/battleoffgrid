1788e2a94709f17fe371a20fc1ca876ab88eaf5c
peanutbutterclassic <rano.r.s.fisher@gmail.com>
2022-01-16 00:14:05 +0000

    Create game introduction to allow user to add name and verify age

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Create game introduction 
+def battle_intro():
+    print("----------Welcome to BattleScrabble!----------")
+    name = input("What's your name? \n")
+    age = int(input("What's your age: \n"))
+    if age >= 18:
+        print("You are eligible to enter the battlefield.")
+    else:
+        print(f"Hello {name.capitalize()}, you are too young to play this game.")
+
